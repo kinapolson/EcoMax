afab62db9eb6cab97f931d836f1a15f4e28cdba7
kinapolson <kpolson824@gmail.com>
2026-01-25 22:08:35 -0500

    gh pages setup

--- constants/theme.ts
+++ constants/theme.ts
@@ -19,7 +19,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#151718',
+    background: '#f5f0e6',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',

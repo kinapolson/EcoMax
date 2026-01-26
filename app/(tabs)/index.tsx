87ce5a1f6508d82827764a0aa39ca34c5b0a10bd
Kina Polson <kpolson824@gmail.com>
2026-01-25 21:59:59 -0500

    Home Page Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,234 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+//import { Platform, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
+//import { HelloWave } from '@/components/hello-wave';
+import { StyleSheet, View, ScrollView } from "react-native";
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">EcoMax!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <ScrollView style={styles.container}>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
+      {/* header */}
+      <ThemedView style={styles.header}>
+        <ThemedText style={styles.headerTxt}>Hi James!</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+
+      {/* eco points/header status */}
+      <View style={styles.cardWrapper}>
+        {/* green bkgd */}
+        <View style={styles.cardBackground} />
+
+        <ThemedView style={styles.topBox}>
+        <View style={styles.topItem}>
+          <ThemedText style={styles.smallTxt}>Eco Points</ThemedText>
+          <ThemedText style={styles.bigText}>350</ThemedText>
+        </View>
+
+        <View style={styles.divider} />
+
+        <View style={styles.topItem}>
+          <ThemedText style={styles.smallTxt}>Eco Challenge</ThemedText>
+          <ThemedText style={styles.bigText}>6/7</ThemedText>
+        </View>
       </ThemedView>
-    </ParallaxScrollView>
+      </View>
+
+      {/* progress bar */}
+      <View style={styles.cardWrapper}>
+        <View style={styles.cardBackground} />
+
+        <ThemedView style={styles.cardContent}>
+          <ThemedText style={styles.cardTitle}>Progress</ThemedText>
+
+          <View style={styles.progressBackground}>
+            <View style={styles.progressFill} />
+            <ThemedText style={styles.percent}>86%</ThemedText>
+          </View>
+        </ThemedView>
+      </View>
+
+      {/* impact tracker */}
+      <View style={styles.cardWrapper}>
+        <View style={styles.cardBackground} />
+
+        {/* impact tracker info */}
+        <ThemedView style={styles.cardContent}>
+          <ThemedText style={styles.cardTitle}>Impact Tracker</ThemedText>
+
+          <View style={styles.chart}>
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 30 }]} />
+              <ThemedText style={styles.day}>Sun</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 70 }]} />
+              <ThemedText style={styles.day}>Mon</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 55 }]} />
+              <ThemedText style={styles.day}>Tue</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 100 }]} />
+              <ThemedText style={styles.day}>Wed</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 40 }]} />
+              <ThemedText style={styles.day}>Thu</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 75 }]} />
+              <ThemedText style={styles.day}>Fri</ThemedText>
+            </View>
+
+            <View style={styles.barGroup}>
+              <View style={[styles.bar, { height: 65 }]} />
+              <ThemedText style={styles.day}>Sat</ThemedText>
+            </View>
+          </View>
+        </ThemedView>
+      </View>
+
+    </ScrollView>
   );
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    backgroundColor: "#f5f0e6",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  header: {
+    paddingVertical: 20,
+    backgroundColor: "#264e36",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
+
+  headerTxt: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+
+  topBox: {
+    backgroundColor: "36db187",
+    borderRadius: 10,
+    flexDirection: "row",
+    padding: 16,
+    marginBottom: 20,
+  },
+
+  topItem: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  smallTxt: {
+    fontSize: 12,
+    color: "#eaf53f",
+  },
+
+  bigText: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+
+  divider: {
+    width: 1,
+    backgroundColor: "#d9efe3",
+  },
+
+  card: {
+    backgroundColor: "36db187",
+    borderRadius: 10,
+    padding: 16,
+    marginBottom: 20,
+  },
+
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    marginBottom: 10,
+  },
+
+  progressBackground: {
+    backgroundColor: "#eaf5ef",
+    height: 30,
+    borderRadius: 20,
+    justifyContent: "center",
+  },
+
+  progressFill: {
+    position: "absolute",
+    height: "100%",
+    width: "86%",
+    backgroundColor: "#264e36",
+    borderRadius: 20,
+  },
+
+  percent: {
+    alignSelf: "flex-end",
+    marginRight: 10,
+    fontWeight: "bold",
+    color: "#5ca377",
+  },
+
+  chart: {
+    backgroundColor: "#161618",
+    borderRadius: 10,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    height: 180,
+  },
+
+  barGroup: {
+    alignItems: "center",
+  },
+
+  bar: {
+    width: 10,
+    backgroundColor: "#5ca377",
+    borderRadius: 5,
+    marginBottom: 5,
+  },
+
+  day: {
+    fontSize: 10,
+    color: "#FFFFFF",
+  },
+
+  cardWrapper: {
+    marginBottom: 20,
+    position: "relative",
+    padding: 10,
+  },
+
+  cardBackground: {
+    position: "absolute",
+    top: 0,
     left: 0,
-    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#5ca377",
+    borderRadius: 12,
   },
+
+  cardContent: {
+    padding: 16,
+    zIndex: 1,
+    backgroundColor: "#5ca377",
+    borderRadius: 12,
+  },
+
 });
